e0fcd4f921ec667a51da88163a1bbabbbfb7eeb4
AVasK <avask17@gmail.com>
2017-04-20 12:45:58 +0300

    Initial Commit

---
+++ GoogleTesting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GoogleTesting
+//
+//  Created by Alex Vaskov on 20.04.17.
+//  Copyright © 2017 AV_Colloque. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
